38bc6c23f9e1bf66287e5a476db49b95f6e69882
Takuya Ito <ito0804takuya@gmail.com>
2024-04-21 21:19:12 +0900

    websocket

--- echo/main.go
+++ echo/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/net/websocket"
 )
 
-// hello-world
+func hello(c echo.Context) error {
+	websocket.Handler(func(ws *websocket.Conn) {
+		// ws接続時のみ実行される
+		defer ws.Close()
+
+		// wsが接続している間、ずっと実行される
+		for {
+			// wsへ送信
+			err := websocket.Message.Send(ws, "Hello!!")
+			if err != nil {
+				c.Logger().Error(err)
+			}
+
+			// 受診
+			msg := ""
+			err = websocket.Message.Receive(ws, &msg)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+			fmt.Printf("%s\n", msg)
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+
+	return nil
+}
+
 func main() {
 	// Echo instance
 	e := echo.New()
@@ -16,10 +42,9 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Route => handler
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
-	})
+	e.Static("/", "public")
+
+	e.GET("/ws", hello)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":5050"))
